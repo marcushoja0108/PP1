0096161b5368abc84699046698b4c058b204ec3a
marcushoja0108 <marcushj@live.no>
2024-11-20 14:21:20 +0100

    Add project files.

---
+++ PP1/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP1
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+        public string Haircolor { get; set; }
+
+        public Person(string name, int age, string hobby, string haircolor)
+        {
+            Name = name;
+            Age = age;
+            Hobby = hobby;
+            Haircolor = haircolor;
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Hei jeg heter {Name}");
+            Console.WriteLine($"Jeg er {Age} år gammel.");
+            Console.WriteLine($"Jeg liker {Hobby}");
+            Console.WriteLine($"jeg har {Haircolor} hår");
+        }
+    }
+}
---
+++ PP1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace PP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool app = true;
+            List<Person> People = new List<Person>();
+            Person person1 = new Person("John", 30, "klatre", "grå");
+            People.Add(person1);
+            while (app == true)
+            {
+                Console.WriteLine("Hva vil du gjøre?");
+                Console.WriteLine("    1. Se alle brukere.");
+                Console.WriteLine("    2. Registrere ny bruker.");
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (userInput == 1)
+                {
+                    Console.WriteLine("Velg en bruker");
+
+                    for (int i = 0; i < People.Count; i++)
+                    {
+                        Console.WriteLine($"    {i}.{People[i].Name}");
+                    }
+                    Console.WriteLine("write the name of the user you want to see"); 
+                    string userinput = Console.ReadLine();
+
+                    foreach (var person in People)
+                    {
+                        if (userinput == person.Name)
+                        {
+                            person.printInfo();
+                        }
+                    }
+                }
+                else if (userInput == 2)
+                {
+                    Console.WriteLine("Hva heter du?");
+                    string name = Console.ReadLine();
+
+                    Console.WriteLine("Hvor gammel er du?");
+                    int age = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Hvilken hårfarge har du?");
+                    string hairColor = Console.ReadLine();
+
+                    Console.WriteLine("Hvilken hobby har du?");
+                    string hobby = Console.ReadLine();
+
+                    Person person2 = new Person(name, age, hobby, hairColor);
+
+                    People.Add(person2);
+                    Thread.Sleep(1000);
+                    Console.WriteLine("User added");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
